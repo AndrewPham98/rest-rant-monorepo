8abb104d5a1135871f7adb02af8feee27120752f
werdnA mahP ✅ <AndrewPham1998@yahoo.com>
2023-12-01 11:55:58 -0800

    user

--- backend/migrations/20210726164104-create-user.js
+++ backend/migrations/20210726164104-create-user.js
@@ -31,3 +31,17 @@ module.exports = {
     await queryInterface.dropTable('users');
   }
 };
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'password_digest', {
+      type: Sequelize.DataTypes.STRING
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('users', 'password_digest')
+  }
+};
