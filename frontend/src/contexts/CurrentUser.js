f8f7a702328f276e896459cc3773cf8b31e94731
werdnA mahP ✅ <AndrewPham1998@yahoo.com>
2023-12-02 18:11:52 -0800

    user

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,11 +1,27 @@
 import { createContext, useState } from "react";
 
 
-export const CurrentUser = createContext()
+export const CurrentUser = createContext(null)
+__
+function CurrentUserProvider({ children }) {
+
+    const [currentUser, setCurrentUser] = useState(null)
+    useEffect(() => {
+
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5000/authentication/profile')
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
+  
+}
 
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
+    
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
@@ -13,5 +29,5 @@ function CurrentUserProvider({ children }){
         </CurrentUser.Provider>
     )
 }
-
+ 
 export default CurrentUserProvider
